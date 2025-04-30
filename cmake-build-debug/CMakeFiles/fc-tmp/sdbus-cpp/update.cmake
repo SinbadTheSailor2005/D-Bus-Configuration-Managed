c21346c492f8fe3460e2872cb8c968c66858f179
Aziz <aziz.yubuza@mail.ru>
2025-04-30 15:17:58 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/fc-tmp/sdbus-cpp/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing update step for sdbus-cpp")
+
+block(SCOPE_FOR VARIABLES)
+
+include("/home/aziz/Projects/D-bus/cmake-build-debug/CMakeFiles/fc-tmp/sdbus-cpp/sdbus-cpp-gitupdate.cmake")
+
+endblock()
---
+++ test.cpp
@@ -0,0 +1,3 @@
+//
+// Created by aziz on 4/30/25.
+//
