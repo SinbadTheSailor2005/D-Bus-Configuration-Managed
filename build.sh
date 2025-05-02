0140641322d5de64f5de065f035f349607b62855
Aziz <aziz.yubuza@mail.ru>
2025-05-02 16:34:21 +0300

    update ./build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# если нет директории - создаем
+source_path=$(pwd)
+if [ ! -d $HOME/con.system.configurationManager ]; then
+mkdir $HOME/com.system.configurationManager
+fi
 
+cd $HOME/com.system.configurationManager 
+echo -e "Timeout:1000\nTimeoutPhrase:hello from aziz" > confManagerApplication1
+cd $source_path
 cmake -S . -B build
 cmake --build build
+  
