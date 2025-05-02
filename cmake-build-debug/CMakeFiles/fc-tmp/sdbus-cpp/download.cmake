04b01904c170052eae9254a814c66764551e1d23
Aziz <aziz.yubuza@mail.ru>
2025-05-02 14:42:30 +0300

    clean up directory attempt #2

--- cmake-build-debug/CMakeFiles/fc-tmp/sdbus-cpp/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for sdbus-cpp")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/aziz/Projects/D-bus/cmake-build-debug/CMakeFiles/fc-tmp/sdbus-cpp/sdbus-cpp-gitclone.cmake")
-
-endblock()
