d53ad2455bcb66649704123453ee30e4762fe436
Aziz <aziz.yubuza@mail.ru>
2025-05-02 14:31:21 +0300

    fix comments in Cmake

--- cmake-build-debug/CMakeFiles/D_bus.dir/src/test.cpp.o.ddi.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/D_bus.dir/src/test.cpp.o.ddi: \
- /home/aziz/Projects/D-bus/src/test.cpp /usr/include/stdc-predef.h
--- cmake-build-debug/CMakeFiles/fc-tmp/json/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for json")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/aziz/Projects/D-bus/cmake-build-debug/CMakeFiles/fc-tmp/json/json-gitclone.cmake")
-
-endblock()
